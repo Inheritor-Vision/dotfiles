ddf7edb6613a868728f0f3fff1618a16858ab30e
Inheritor-Vision <valentin.boulou@gmail.com>
2022-06-07 00:32:25 +0200

    ChangeColorScheme: Handle new version & update instead of creating new ones

--- .config/qtile/theme_generator/change_color_scheme.py
+++ .config/qtile/theme_generator/change_color_scheme.py
@@ -1,8 +1,8 @@
 import json
 import subprocess
 
-from tinydb import TinyDB, Query
 from hashlib import sha256
+from tinydb import TinyDB, Query
 
 class ColorSchemeUnifier():
 
@@ -15,12 +15,17 @@ class ColorSchemeUnifier():
     DB_DEFAULT_TABLE    = "image_palette"
  
     db                  = None
-
+    colorpalette_ver    = None
 
     def __init__(self):
         self.db                     = TinyDB(self.DB_NAME)
         self.db.default_table_name  = self.DB_DEFAULT_TABLE
 
+        self.colorpalette_ver       = subprocess.check_output([
+            "node",
+            self.PATH_COLORPALETTE,
+            "-v"
+        ]).decode().replace("\n", "")
 
     def change_theme(self, image):
         image_hash = sha256(open(image, "rb").read()).hexdigest()
@@ -32,8 +37,8 @@ class ColorSchemeUnifier():
     def _get_color_scheme(self, image, image_hash):
         res = self.db.search(Query().image_hash == image_hash)
 
-        if res:
-            return res[0]["palette"] 
+        if res and res[0]["colorpalette_ver"] == self.colorpalette_ver:
+            return res[0]["palette"]    
         else:
             output = subprocess.check_output(
                     (
@@ -45,9 +50,17 @@ class ColorSchemeUnifier():
                     )
                 )
             palette = json.loads(output)["palette"]
-            self._save_color_scheme(image_hash, palette)
+
+            if res and res[0]["colorpalette_ver"] != self.colorpalette_ver:
+                self._update_color_scheme(image_hash, palette)
+            else:
+                self._save_color_scheme(image_hash, palette)
+            
             return palette
 
+    def _update_color_scheme(self, image_hash, palette):
+        self.db.update({"colorpalette_ver": self.colorpalette_ver, "palette": palette}, Query().image_hash == image_hash)
+
     def _save_color_scheme(self, image_hash, palette):
-        self.db.insert({"image_hash": image_hash, "palette": palette})
+        self.db.insert({"image_hash": image_hash, "palette": palette, "colorpalette_ver": self.colorpalette_ver})
 
